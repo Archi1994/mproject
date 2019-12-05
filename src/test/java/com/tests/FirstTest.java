44e5d52d8b61a1a166c0db6fb66e37da6c663708
Archi1994 <archanataneja17@gmail.com>
2019-12-05 18:24:01 +0530

    my first commit to correct project

---
+++ src/test/java/com/tests/FirstTest.java
@@ -0,0 +1,36 @@
+package com.tests;
+
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+
+public class FirstTest {
+	
+  WebDriver driver;
+  @Test
+  public void f() {
+	  WebElement email = driver.findElement(By.cssSelector("input[type='email']"));
+      email.sendKeys("archanataneja@gmail.com");
+  }
+  @BeforeTest
+  public void beforeTest() {
+	  WebDriverManager.chromedriver().setup();
+      
+      driver=new ChromeDriver();
+      driver.get("http://www.facebook.com");
+      driver.manage().window().maximize();
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  driver.quit();
+  }
+
+}
